15180bedba9a0e19eeae76b79736ea95a59ab3ce
adam <adam.jenkins@smartsti.com>
2019-02-20 22:21:40 -0400

    Forgot some files...

--- rollup.config.js
+++ rollup.config.js
@@ -6,19 +6,33 @@ import builtins from "rollup-plugin-node-builtins";
 import pkg from "./package.json";
 
 export default [
-  // Minified with dependencies - suitable for environments with a fetch
+  // Minified with dependencies - suitable for all environments with a fetch
   {
     input: "src/flighty.js",
     output: {
       file: "lib/flighty.min.js",
       format: "iife",
       name: "flighty",
-      globals: { flighty: "flighty" }
+      globals: { flighty: "flighty" },
+      sourcemap: true
     },
     external: [],
     plugins: [babel(), resolve(), commonjs(), minify(), builtins()]
   },
-  // Without dependencies - suitable for webpack-ed front ends
+  // Minified with dependencies AND fetch - suitable for browser environments where fetch is unknown
+  {
+    input: "src/flighty.fetch.js",
+    output: {
+      file: "lib/flighty.fetch.min.js",
+      format: "iife",
+      name: "flighty",
+      globals: { flighty: "flighty" },
+      sourcemap: true
+    },
+    external: [],
+    plugins: [babel(), resolve(), commonjs(), builtins(), minify()]
+  },
+  // Without dependencies - suitable for all ES5 environments
   {
     input: "src/flighty.js",
     output: {
@@ -32,17 +46,5 @@ export default [
       ...Object.keys(pkg.peerDependencies || {})
     ],
     plugins: [babel()]
-  },
-  // Minified with dependencies AND fetch - suitable for older browsers
-  {
-    input: "src/flighty.fetch.js",
-    output: {
-      file: "lib/flighty.fetch.min.js",
-      format: "iife",
-      name: "flighty",
-      globals: { flighty: "flighty" }
-    },
-    external: [],
-    plugins: [babel(), resolve(), commonjs(), builtins(), minify()]
   }
 ];
